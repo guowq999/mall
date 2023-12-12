5463ad42d7a6d7a8838a5cd221566cbc5c77dfaa
wenqiang <wenqiangG11@163.com>
2023-12-12 17:51:12 +0800

    feat:项目初始化

---
+++ mall-coupon/src/test/java/com/owen/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-order/src/test/java/com/owen/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
